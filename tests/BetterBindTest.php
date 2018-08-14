96d7cb62cdf017b242966ca811b0fd71db0a4493
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2018-08-14 11:24:05 -0400

    These classes should be separate

--- tests/BetterBindTest.php
+++ tests/BetterBindTest.php
@@ -41,8 +41,8 @@ class BetterBindTest extends TestCase
     public function testBindHeadlessClassNoParams()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedNoParams::class, function () use ($object) { return $object; }, $params);
-        $got = App::makeWith(MyBindTest_NeedNoParams::class, []);
+        $this->appBind(INeedNoParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(INeedNoParams::class, []);
         $this->assertEquals($object, $got);
         $this->assertCount(0, $params);
     }
@@ -55,13 +55,13 @@ class BetterBindTest extends TestCase
     public function testBindHeadlessClassWithParams()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedNoParams::class, function () use ($object) { return $object; }, $params);
+        $this->appBind(INeedNoParams::class, function () use ($object) { return $object; }, $params);
         try {
-            App::makeWith(MyBindTest_NeedNoParams::class, ['y' => 'z']);
+            App::makeWith(INeedNoParams::class, ['y' => 'z']);
         } catch (PHPUnit_Framework_ExpectationFailedException $e) {
         }
         $this->assertNotNull($e);
-        $this->assertRegExp('/MyBindTest_NeedNoParams/', $e->getMessage());
+        $this->assertRegExp('/INeedNoParams/', $e->getMessage());
         $this->assertRegExp('/no constructor/', $e->getMessage());
         $this->assertEquals('z', $params['y']);
         $this->assertCount(1, $params);
@@ -74,8 +74,8 @@ class BetterBindTest extends TestCase
     public function testBindHeadfulClassWithAllParams()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
-        $got = App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123, 'second_param' => 456]);
+        $this->appBind(INeedParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(INeedParams::class, ['first_param' => 123, 'second_param' => 456]);
         $this->assertEquals($object, $got);
         $this->assertEquals(123, $params['first_param']);
         $this->assertEquals(456, $params['second_param']);
@@ -89,8 +89,8 @@ class BetterBindTest extends TestCase
     public function testBindHeadfulClassWithOneParam()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
-        $got = App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123]);
+        $this->appBind(INeedParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(INeedParams::class, ['first_param' => 123]);
         $this->assertEquals($object, $got);
         $this->assertEquals(123, $params['first_param']);
         $this->assertCount(1, $params);
@@ -103,13 +103,13 @@ class BetterBindTest extends TestCase
     public function testBindHeadfulClassNoParams()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        $this->appBind(INeedParams::class, function () use ($object) { return $object; }, $params);
         try {
-            App::makeWith(MyBindTest_NeedParams::class, []);   
+            App::makeWith(INeedParams::class, []);   
         } catch (PHPUnit_Framework_ExpectationFailedException $e) {
         }
         $this->assertNotNull($e);
-        $this->assertRegExp('/MyBindTest_NeedParams/', $e->getMessage());
+        $this->assertRegExp('/INeedParams/', $e->getMessage());
         $this->assertRegExp('/Required parameter/', $e->getMessage());
         $this->assertCount(0, $params);
     }
@@ -121,13 +121,13 @@ class BetterBindTest extends TestCase
     public function testBindHeadfulClassWithTooManyParams()
     {
         $object = new stdClass();
-        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        $this->appBind(INeedParams::class, function () use ($object) { return $object; }, $params);
         try {
-            App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123, 'second_param' => 456, 'failure_param' => 789]);   
+            App::makeWith(INeedParams::class, ['first_param' => 123, 'second_param' => 456, 'failure_param' => 789]);   
         } catch (PHPUnit_Framework_ExpectationFailedException $e) {
         }
         $this->assertNotNull($e);
-        $this->assertRegExp('/MyBindTest_NeedParams/', $e->getMessage());
+        $this->assertRegExp('/INeedParams/', $e->getMessage());
         $this->assertRegExp('/failure_param/', $e->getMessage());
         $this->assertEquals(123, $params['first_param']);
         $this->assertEquals(456, $params['second_param']);
@@ -143,16 +143,5 @@ class BetterBindTest extends TestCase
         $this->assertEquals($object, $got);
         $this->assertEquals($params['y'], 'z');
         $this->assertCount(1, $params);
-    }
-}
-
-class MyBindTest_NeedNoParams
-{
-}
-
-class MyBindTest_NeedParams
-{
-    public function __construct($first_param, $second_param = 'default')
-    {
     }
 }
---
+++ tests/fixtures/INeedParams.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class INeedParams
+{
+    public function __construct($first_param, $second_param = 'default')
+    {
+    }
+}

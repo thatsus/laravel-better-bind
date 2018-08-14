94189d6bf4ce589e5553db0734c060ec4cc642aa
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2018-08-14 11:13:50 -0400

    BetterBind!

---
+++ src/BetterBind.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ThatsUs;
+
+use Illuminate\Support\Facades\App;
+use ReflectionClass;
+use Closure;
+
+trait BetterBind
+{
+
+    public function appBind(string $signature, Closure $closure, &$caught_params = [])
+    {
+        App::bind($signature, function ($app, $params) use ($signature, $closure, &$caught_params) {
+            $caught_params = $params;
+            if (class_exists($signature)) {
+                $this->assertParamsMatchConstructor($signature, $params);
+            }
+            return $closure($app, $params);
+        });
+    }
+
+    public function appInstance(string $signature, $instance, &$caught_params = [])
+    {
+        $this->appBind($signature, function () use ($instance) { return $instance; }, $caught_params);
+    }
+
+    public function assertParamsMatchConstructor(string $class_name, array $params)
+    {
+        $class = new ReflectionClass($class_name);
+        $constructor = $class->getConstructor();
+        if (!$constructor) {
+            $this->assertCount(0, $params, "Parameters provided to class with no constructor: `{$class_name}`");
+        } else {
+            collect($constructor->getParameters())
+                ->each(function ($parameter) use (&$params, $class_name) {
+                    $name = $parameter->getName();
+                    if (!$parameter->isOptional()) {
+                        $this->assertTrue(isset($params[$name]), "Required parameter `{$name}` not provided to class constructor for `{$class_name}`");
+                    }
+                    unset($params[$name]);
+                });
+            $extra_params = collect($params)
+                ->keys()
+                ->map(function ($param) {
+                    return "`{$param}`";
+                })
+                ->implode(', ');
+            $this->assertCount(0, $params, "Extra parameters provided to class constructor for `{$class_name}`: {$extra_params}");
+        }
+    }
+}
---
+++ tests/BetterBindTest.php
@@ -0,0 +1,158 @@
+<?php
+
+class BetterBindTest extends TestCase
+{
+
+    use \ThatsUs\BetterBind;
+
+    /**
+     * A non-class-name is used for the signature, so we don't expect any 
+     * trouble from constructor params.
+     * No params are passed in.
+     */
+    public function testBindNonsenseWordNoParams()
+    {
+        $object = new stdClass();
+        $this->appBind('x', function () use ($object) { return $object; }, $params);
+        $got = App::makeWith('x', []);
+        $this->assertEquals($object, $got);
+        $this->assertCount(0, $params);
+    }
+
+    /**
+     * A non-class-name is used for the signature, so we don't expect any 
+     * trouble from constructor params.
+     * Params are passed in and received.
+     */
+    public function testBindNonsenseWordAndParams()
+    {
+        $object = new stdClass();
+        $this->appBind('x', function () use ($object) { return $object; }, $params);
+        $got = App::makeWith('x', ['y' => 'z']);
+        $this->assertEquals($object, $got);
+        $this->assertEquals($params['y'], 'z');
+        $this->assertCount(1, $params);
+    }
+
+    /**
+     * A class that needs no params.
+     * No params are passed in.
+     */
+    public function testBindHeadlessClassNoParams()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedNoParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(MyBindTest_NeedNoParams::class, []);
+        $this->assertEquals($object, $got);
+        $this->assertCount(0, $params);
+    }
+
+    /**
+     * A class that needs no params.
+     * Params are passed in. (Failure!)
+     * Params are caught even though there is failure.
+     */
+    public function testBindHeadlessClassWithParams()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedNoParams::class, function () use ($object) { return $object; }, $params);
+        try {
+            App::makeWith(MyBindTest_NeedNoParams::class, ['y' => 'z']);
+        } catch (PHPUnit_Framework_ExpectationFailedException $e) {
+        }
+        $this->assertNotNull($e);
+        $this->assertRegExp('/MyBindTest_NeedNoParams/', $e->getMessage());
+        $this->assertRegExp('/no constructor/', $e->getMessage());
+        $this->assertEquals('z', $params['y']);
+        $this->assertCount(1, $params);
+    }
+
+    /**
+     * A class that needs some params.
+     * All params are passed in.
+     */
+    public function testBindHeadfulClassWithAllParams()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123, 'second_param' => 456]);
+        $this->assertEquals($object, $got);
+        $this->assertEquals(123, $params['first_param']);
+        $this->assertEquals(456, $params['second_param']);
+        $this->assertCount(2, $params);
+    }
+
+    /**
+     * A class that needs some params.
+     * Only one param is passed in, the other can be default.
+     */
+    public function testBindHeadfulClassWithOneParam()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        $got = App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123]);
+        $this->assertEquals($object, $got);
+        $this->assertEquals(123, $params['first_param']);
+        $this->assertCount(1, $params);
+    }
+
+    /**
+     * A class that needs some params.
+     * No params are passed in (FAILURE!)
+     */
+    public function testBindHeadfulClassNoParams()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        try {
+            App::makeWith(MyBindTest_NeedParams::class, []);   
+        } catch (PHPUnit_Framework_ExpectationFailedException $e) {
+        }
+        $this->assertNotNull($e);
+        $this->assertRegExp('/MyBindTest_NeedParams/', $e->getMessage());
+        $this->assertRegExp('/Required parameter/', $e->getMessage());
+        $this->assertCount(0, $params);
+    }
+
+    /**
+     * A class that needs some params.
+     * Too many params are passed in.
+     */
+    public function testBindHeadfulClassWithTooManyParams()
+    {
+        $object = new stdClass();
+        $this->appBind(MyBindTest_NeedParams::class, function () use ($object) { return $object; }, $params);
+        try {
+            App::makeWith(MyBindTest_NeedParams::class, ['first_param' => 123, 'second_param' => 456, 'failure_param' => 789]);   
+        } catch (PHPUnit_Framework_ExpectationFailedException $e) {
+        }
+        $this->assertNotNull($e);
+        $this->assertRegExp('/MyBindTest_NeedParams/', $e->getMessage());
+        $this->assertRegExp('/failure_param/', $e->getMessage());
+        $this->assertEquals(123, $params['first_param']);
+        $this->assertEquals(456, $params['second_param']);
+        $this->assertEquals(789, $params['failure_param']);
+        $this->assertCount(3, $params);
+    }
+
+    public function testInstance()
+    {
+        $object = new stdClass();
+        $this->appInstance('x', $object, $params);
+        $got = App::makeWith('x', ['y' => 'z']);
+        $this->assertEquals($object, $got);
+        $this->assertEquals($params['y'], 'z');
+        $this->assertCount(1, $params);
+    }
+}
+
+class MyBindTest_NeedNoParams
+{
+}
+
+class MyBindTest_NeedParams
+{
+    public function __construct($first_param, $second_param = 'default')
+    {
+    }
+}
